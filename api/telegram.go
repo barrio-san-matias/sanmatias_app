4984038a6d92c207a6daf298ead3dfd56b933724
jf <jf@jfs-macbook-air.lan>
2022-05-15 19:23:18 -0300

    init telegram handler

--- api/telegram.go
+++ api/telegram.go
@@ -1,13 +1,43 @@
 package handler
 
 import (
-    "fmt"
-    "net/http"
-    "time"
+	"fmt"
+	"log"
+	"net/http"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joeshaw/envdecode"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("incoming request...")
-    currentTime := time.Now().Format(time.RFC850)
-    fmt.Fprintf(w, currentTime)
+	fmt.Println("incoming request")
+
+	var cfg struct {
+		TelegramToken string `env:"TELEGRAM_TOKEN,required"`
+	}
+	if err := envdecode.StrictDecode(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	bot, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	update, err := bot.HandleUpdate(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Hi there %s", update.Message.From.UserName))
+	msg.ReplyToMessageID = update.Message.MessageID
+
+	_, err = bot.Send(msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprint(w, "OK")
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github/jfatta/smbot
+
+go 1.18
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd // indirect
+)
