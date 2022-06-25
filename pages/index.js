8d4894734ce932d2cab6cc489bb5bb9a230f5430
Jorge L. Fatta <jorgefatta@gmail.com>
2022-06-25 15:47:48 -0300

    adding poi

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,13 @@
 import Head from 'next/head'
-import Image from 'next/image'
-
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function PageWithJSbasedForm() {
-  // Handle the submit event on form submit.
-  const handleSubmit = async (event) => {
-    // Stop the form from submitting and refreshing the page.
+  const searchLote = async (event) => {
     event.preventDefault()
-
-
-    // Send the form data to our API and get a response.
     const response = await fetch(`/api/map?lote=${event.target.lote.value}`, {
-      // Tell the server we're sending JSON.
       headers: {
         'Content-Type': 'application/json',
       },
-      // The method is POST because we are sending data.
       method: 'GET',
     })
 
@@ -25,12 +15,30 @@ export default function PageWithJSbasedForm() {
       const text = await response.text()
       window.alert(text)
     } else {
-    // Get the response data from server as JSON.
-    // If server returns the name submitted, that means the form works.
     const result = await response.json()
      window.location.replace(result.MapURL);
     }
   }
+
+  const searchPOI = async (event) => {
+    event.preventDefault()
+    const response = await fetch(`/api/map?poi=${event.target.poi.className}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    })
+
+    if (!response.ok) {
+      const text = await response.text()
+      window.alert(text)
+    } else {
+    const result = await response.json()
+     window.location.replace(result.MapURL);
+    }
+  }
+
+
   return (
     <div className="container">
       <Head>
@@ -39,16 +47,29 @@ export default function PageWithJSbasedForm() {
       </Head>
       <h1 className={styles.title}>
         Mapa de San Matías
-       </h1>
+      </h1>
 
       <p className={styles.description}>
         A qué lote vas? 
       </p>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={searchLote}>
         <input type="number" id="lote" name="lote" required />
         <button type="submit">buscar</button>
       </form>
+
+      <div className={styles.poi}>
+        <p className={styles.description}>
+          otros puntos de interes: 
+        </p>
+        <form onSubmit={searchPOI}>
+          <button type="submit" id="poi" className="sum">SUM</button>
+        </form>
+        <form onSubmit={searchPOI}>
+          <button type="submit" id="poi" className="adm">Administracion</button>
+        </form>
+
+      </div>
 
       <div className="footer">
         <div>hecho por <a href="mailto:notjorge@protonmail.com">notjorge@protonmail.com</a></div>
