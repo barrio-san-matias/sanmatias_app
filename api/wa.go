5368d29fd4fc423f2f7aaf648ff75e1731f36eb7
Jorge L. Fatta <jorge.fatta@auth0.com>
2023-05-31 10:01:38 +0000

    wip

--- api/wa.go
+++ api/wa.go
@@ -1,32 +1,32 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
-	"fmt"
-	"log"
-	"net/http"
-
+	"github.com/joeshaw/envdecode"
 	"github.com/twilio/twilio-go"
-	"github.com/twilio/twilio-go/rest/api/v2010"
+	twilioApi "github.com/twilio/twilio-go/rest/api/v2010"
 )
-
-const (
-	token_secret = "verify"
-)
-
-//type WebhookMessage map[string]interface{}
 
 func WhatsAppHandler(w http.ResponseWriter, r *http.Request) {
+	var cfg struct {
+		TwilioToken string `env:"TWILIO_TOKEN,required"`
+		TwilioSID   string `env:"TWILIO_SID,required"`
+	}
+	if err := envdecode.StrictDecode(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
 	// Set your Twilio Account SID and Auth Token
-	accountSid := "YOUR_TWILIO_ACCOUNT_SID"
-	authToken := "YOUR_TWILIO_AUTH_TOKEN"
+	accountSid := cfg.TwilioSID
+	authToken := cfg.TwilioToken
 
 	// Create a new Twilio client
-	client := twilio.NewRestClient(accountSid, authToken)
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
 
 	// Get the country from the request body
 	err := r.ParseForm()
@@ -37,44 +37,10 @@ func WhatsAppHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	country := r.FormValue("Body")
 
-	// Retrieve data from the API
-	resp, err := client.Get(fmt.Sprintf("https://restcountries.eu/rest/v2/name/%s?fullText=true", country), nil)
-	if err != nil {
-		log.Println("Failed to retrieve data from API:", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-
 	// Create a new TwiML response
-	response := api.NewMessageResponse()
+	response := twilioApi.NewMessageResponse()
 
-	// Check the response status code
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		log.Printf("Failed to retrieve data for the following country - %s. Here is a more verbose reason: %s\n", country, resp.Status)
-		response.SetBody("Sorry we could not process your request. Please try again or check a different country")
-	} else {
-		// Parse the response JSON
-		var data []map[string]interface{}
-		err = json.NewDecoder(resp.Body).Decode(&data)
-		if err != nil {
-			log.Println("Failed to decode JSON:", err)
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-
-		if len(data) > 0 {
-			countryData := data[0]
-			nativeName := countryData["nativeName"].(string)
-			capital := countryData["capital"].(string)
-			people := countryData["demonym"].(string)
-			region := countryData["region"].(string)
-
-			response.SetBody(fmt.Sprintf("%s is a country in %s. Its capital city is %s, while its native name is %s. A person from %s is called a %s.", country, region, capital, nativeName, country, people))
-		} else {
-			response.SetBody("No data found for the specified country")
-		}
-	}
+	response.SetBody("HI!!!!!")
 
 	// Write the response
 	w.Header().Set("Content-Type", "application/xml")
