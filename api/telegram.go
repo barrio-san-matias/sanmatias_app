04345b10bf95c0a7e11fedb246ac0198e6a28961
Jorge L. Fatta <jorgefatta@gmail.com>
2023-06-03 01:01:42 -0300

    validate user

--- api/telegram.go
+++ api/telegram.go
@@ -2,49 +2,46 @@ package handler
 
 import (
 	"context"
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
+	"time"
 
+	"github.com/davecgh/go-spew/spew"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joeshaw/envdecode"
 	redis "github.com/redis/go-redis/v9"
 )
 
-func TelegramHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("incoming request")
+type User struct {
+	Id       string `redis:"id"`
+	Phone    string `redis:"phone"`
+	Name     string `redis:"name"`
+	LastName string `redis:"last_name"`
+	Username string `redis:"username"`
 
+	CertifiedTime time.Time `redis:"certified_time"`
+
+	CreateTime time.Time `redis:"create_time"`
+	UpdateTime time.Time `redis:"update_time"`
+}
+
+func TelegramHandler(w http.ResponseWriter, r *http.Request) {
 	var cfg struct {
 		TelegramToken string `env:"TELEGRAM_TOKEN,required"`
 		KVUrl         string `env:"KV_URL,required"`
-		KVToken       string `env:"KV_REST_API_TOKEN,required"`
 	}
+
 	if err := envdecode.StrictDecode(&cfg); err != nil {
 		log.Fatal(err)
 	}
 
 	ctx := context.Background()
 
-	redisUrl, err := url.Parse(cfg.KVUrl)
+	kv, err := getRedisClient(cfg.KVUrl)
 	if err != nil {
-		log.Fatalf(">>> REDIS Url Parse: %+v", err)
-	}
-
-	redisPassword, _ := redisUrl.User.Password()
-	redisUser := redisUrl.User.Username()
-
-	opt := redis.Options{
-		Addr:     redisUrl.Host,
-		Password: cfg.KVToken,
-		DB:       0,
-	}
-
-	rdb := redis.NewClient(&opt)
-
-	err = rdb.Set(ctx, "key", "value", 0).Err()
-	if err != nil {
-		log.Fatalf(">>> REDIS ERROR: %+v. Usr: %s, Pwd: %s, Host: %s", err, redisUser, redisPassword, redisUrl.Host)
+		log.Fatalf("cannot get kv client: %v", err)
 	}
 
 	bot, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
@@ -59,47 +56,115 @@ func TelegramHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	fmt.Printf(">>>>>>> UPDATE: %+v\n", update)
-	if update.Message.Contact != nil {
-		fmt.Printf(">>>>>>> CONTACT: %+v\n", update.Message.Contact.PhoneNumber)
-	}
-
-	// si empieza con "lote <numero>" devuelve la ubicacion,
-	// sino tira la consulta a dialogflow
-	if update.Message.Text == "/lote 636" {
-		msg := tgbotapi.NewVenue(
-			update.Message.Chat.ID,
-			"lote 636",
-			"(tocar el mapa para abrir y navegar)",
-			-34.3507263,
-			-58.7637032,
-		)
-
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		_, err = bot.Send(msg)
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		msg := tgbotapi.NewMessage(
-			update.Message.Chat.ID,
-			fmt.Sprintf(">>> sender: %+v\n, update:%+v", update.Message.From, update),
-		)
-
-		msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(
-			tgbotapi.NewKeyboardButtonRow(
-				tgbotapi.KeyboardButton{
-					Text:           "compartir numero?",
-					RequestContact: true,
-				}))
-
-		_, err = bot.Send(msg)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-	}
-
 	fmt.Fprint(w, "OK")
+
+	u, err := upsertUser(ctx, kv, update)
+	if err != nil {
+		panic(err)
+	}
+
+	var msg tgbotapi.MessageConfig
+	if u == nil || u.Phone == "" {
+		msg = getContactRequestMessage(update)
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	if u != nil && u.CertifiedTime.IsZero() {
+		msg = getAccessDeniedMessage(update)
+		_, err = bot.Send(msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	// handle bot request, user is authorized:
+	msg = getResponseMessage(update, "TO-DO")
+	_, err = bot.Send(msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getContactRequestMessage(update *tgbotapi.Update) tgbotapi.MessageConfig {
+	msg := tgbotapi.NewMessage(
+		update.Message.Chat.ID,
+		"Bienvenido al Bot de San Matias. Solo vecinos certificados.")
+
+	msg.ReplyMarkup = tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.KeyboardButton{
+				Text:           "compartir numero?",
+				RequestContact: true,
+			}))
+
+	return msg
+}
+
+func getAccessDeniedMessage(update *tgbotapi.Update) tgbotapi.MessageConfig {
+	msg := tgbotapi.NewMessage(
+		update.Message.Chat.ID,
+		"⛔️ Su numero de telefono no esta registrado como un vecino de San Matias.")
+
+	return msg
+}
+
+func getResponseMessage(update *tgbotapi.Update, res string) tgbotapi.MessageConfig {
+	msg := tgbotapi.NewMessage(
+		update.Message.Chat.ID,
+		res)
+
+	return msg
+}
+
+func getRedisClient(u string) (*redis.Client, error) {
+	opt, err := redis.ParseURL(u)
+	if err != nil {
+		return nil, err
+	}
+
+	opt.TLSConfig = &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	rdb := redis.NewClient(opt)
+
+	return rdb, nil
+}
+
+func upsertUser(ctx context.Context, kv *redis.Client, update *tgbotapi.Update) (*User, error) {
+	if update == nil {
+		return nil, nil
+	}
+
+	spew.Dump(update)
+
+	id := fmt.Sprint(update.Message.From.ID)
+
+	var user *User = &User{}
+	// Scan all fields into the model.
+	if err := kv.HGetAll(ctx, id).Scan(&user); err != nil {
+		return nil, err
+	}
+
+	if user == nil {
+		user = &User{
+			Id: id,
+		}
+	}
+
+	if update.Message.Contact != nil {
+		user.Phone = update.Message.Contact.PhoneNumber
+	}
+
+	user.UpdateTime = time.Now()
+
+	// save user
+	kv.HSet(ctx, id, user)
+
+	return user, nil
 }
