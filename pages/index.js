4fa1d9f08fde402c60339d6390ddc4ec93694757
Jorge L. Fatta <jorge.fatta@auth0.com>
2023-06-05 10:41:51 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function PageWithJSbasedForm() {
       <h1 className={styles.title}>
         Mapa de San Matías
       </h1>
-      <h4 className={styles.dislaimer}>
+      <h4 className={styles.disclaimer}>
       nota: <b>herramienta privada no asociada</b> a la administración, desarrolladores, o comisión de vecinos del barrio San Matías. <br />
               Consultas: <a href="mailto:hi@jorgefatta.dev">hi@jorgefatta.dev</a>
       </h4>
