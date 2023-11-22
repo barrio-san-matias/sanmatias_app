15c8446e368b6bd98f5060dbedae7ff41c2a4f6b
Jorge L. Fatta <jorge.fatta@auth0.com>
2023-11-22 00:17:13 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -85,10 +85,12 @@ export default function PageWithJSbasedForm() {
       </div>
 
       <div className="footer">
-        <div>🏴 Hecho por <a href="mailto:hi@jorgefatta.dev">hi@jorgefatta.dev</a> 🇦🇷 </div>
-        <div><a href="https://cafecito.app/defnotjorge">invitame un café</a></div>
+        <div> Hecho por <a href="mailto:hi@jorgefatta.dev">hi@jorgefatta.dev</a> </div>
+        
         <div>v1.0.0</div> 
-      </div>
+        <div><a href='https://cafecito.app/defnotjorge' rel='noopener' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_5.png 1x, https://cdn.cafecito.app/imgs/buttons/button_5_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_5_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_5.png' alt='Invitame un café en cafecito.app' /></a>
+     </div>
+       </div>
     </div>
   )
 }
