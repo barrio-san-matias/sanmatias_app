26f9bbae5a132ddf942890fd24bb8dbfff406b26
Jorge L. Fatta <jorgefatta@gmail.com>
2024-04-16 23:01:22 -0300

    add bot

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,25 @@ export default function PageWithJSbasedForm() {
         />
       </Head>
       <h1 className={styles.title}>maps.sanmatias.app</h1>
-
+      <script>
+  (function (w, d, s, o, f, js, fjs) {
+    w["botsonic_widget"] = o;
+    w[o] =
+      w[o] ||
+      function () {
+        (w[o].q = w[o].q || []).push(arguments);
+      };
+    (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
+    js.id = o;
+    js.src = f;
+    js.async = 1;
+    fjs.parentNode.insertBefore(js, fjs);
+  })(window, document, "script", "Botsonic", "https://widget.writesonic.com/CDN/botsonic.min.js");
+  Botsonic("init", {
+    serviceBaseUrl: "https://api-azure.botsonic.ai",
+    token: "5fc48023-f0a6-4283-b38d-5d6db2e2ae87",
+  });
+</script>
       <div className="mapTypes">
         <RadioGroup
           selectedOption={selectedValue}
