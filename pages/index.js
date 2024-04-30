ce5490cdf893d33f0183ba58a28492957f17b63b
Jorge L. Fatta <jorge.fatta@auth0.com>
2024-04-30 09:10:50 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -122,12 +122,9 @@ export default function PageWithJSbasedForm() {
           </button>
         </form>
       </div>
-
-      <script type='module' src='https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js'></script>
-      <zapier-interfaces-chatbot-embed is-popup='true' chatbot-id='clv39mlqb000ptke6l3d6khuq' height='600px' width='400px'></zapier-interfaces-chatbot-embed>
-            
+   
       <div className="footer">
-        <a href="mailto:hi@jorgefatta.dev"> hi@jorgefatta.dev - v1.2.3 </a>
+        <a href="mailto:hi@jorgefatta.dev"> hi@jorgefatta.dev - v1.2.4 </a>
       </div>
       <Analytics />
       <SpeedInsights />
